5319ed04bb5b84536c53bd8e285907b4629c3c0b
Michael Lamertz <michael.lamertz@gmail.com>
2023-02-16 14:13:19 +0100

    Untested! Make setup.py handle statics and templates

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
-    name="subnautica-navigator",
-    version="0.1.0",
-    packages=[],
+    name='sncompass',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'flask-wtf',
+        'pymongo',
+        'tinysvg @ git+https://github.com/DickerDackel/tinysvg',
+        'gunicorn',
+    ],
     entry_points={
         'console_scripts': [
             'sncompass = sncompass.server:main',
